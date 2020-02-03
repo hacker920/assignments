c1cfb73eeb567a587f34cd58714ed23005805ab2
hacker920 <34811014+hacker920@users.noreply.github.com>
2020-02-03 15:43:55 +0530

    Add files via upload

---
+++ assignment1/Main.java
@@ -0,0 +1,55 @@
+package assignment1;
+
+import assignment1.models.*;
+import java.util.Scanner;
+
+public class Main {
+    static Scanner sc = new Scanner(System.in);
+    public static void main(String args[]){
+        System.out.println("Enter the item details ");
+        Boolean flag = true;
+        ItemCollection items = new ItemCollection();
+
+        while(true){
+            System.out.print(" -name : ");
+            String name  = sc.next();
+            if( name == null || name.isEmpty() ){
+                System.out.println("name should not be null or empty");
+                continue;
+            }
+
+            System.out.print(" -price : ");
+            Double price = sc.nextDouble();
+
+            System.out.print(" -quantity : ");
+            int quantity = sc.nextInt();
+
+            System.out.print(" -type : ");
+            String type = sc.next();
+            try {
+                Item item = null;
+                if("raw".equals(type)) {
+                    item = new RawItem(name, price, quantity);
+                }else if("manufactured".equals(type)){
+                    item = new ManufacturedItem(name, price, quantity);
+                }else if("imported".equals(type)){
+                    item = new ImportedItem(name,price,quantity);
+                }else{
+                    System.out.println("type is mandatory and should raw/manufactured/imported");
+                    continue;
+                }
+                items.addItem(item);
+                System.out.println("done : item added");
+            }catch (Exception e){
+                System.out.println(" failed : item is not added ");
+                e.printStackTrace();
+            }
+            System.out.print("Do you want to add more items? (y/n)");
+            String choice = sc.next();
+            if(choice.equals("n")){
+                break;
+            }
+        }
+        items.displayItem();
+    }
+}
